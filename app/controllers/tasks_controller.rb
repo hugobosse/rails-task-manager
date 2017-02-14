cd757a3067fbbea66d7c955082fb7160417cd485
hugo bosse <monsieurbosse@gmail.com>
2017-02-14 13:04:27 +0100

    added the form partial

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,17 +9,27 @@ class TasksController < ApplicationController
 
   def new
               # GET /tasks/new
+    @task = TaskManager.new
   end
 
   def create        # POST /tasks
+    @task = TaskManager.new(params[:task])
+    @task.save
   end
 
   def edit          # GET /tasks/:id/edit
   end
 
   def update        # PATCH /tasks/:id
+    @task = TaskManager.find(params[:id])
+    @task.update(params[:task])
   end
 
   def destroy       # DELETE /tasks/:id
+    @task = TaskManager.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
   end
 end
